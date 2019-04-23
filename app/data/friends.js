99030c47b7eaa43b7793b9b0e7ef985fc3784d66
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-04-23 11:22:08 -0500

    final before heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-
+// friends array of objects which contain information on all of your friends
 var friends = [
     {
         name: "Jerry",
@@ -22,12 +22,34 @@ var friends = [
     },
     {
         name: "Sampson",
-        photo: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        photo: "https://images.pexels.com/photos/936229/pexels-photo-936229.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
         scores: [3,5,4,2,4,3,5,3,1,3]
+    },
+    {
+        name: "Chloe",
+        photo: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "Tim",
+        photo: "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        scores: [4,4,4,4,4,4,4,4,4,4]
+    },
+    {
+        name: "Max",
+        photo: "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&h=650&w=940",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: "Jimbob",
+        photo: "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        scores: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: "Reia",
+        photo: "https://images.pexels.com/photos/638700/pexels-photo-638700.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        scores: [1,1,1,1,1,1,1,1,1,1]
     }
 ];
-
-
-
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,20 +13,22 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        
+        //take in the request body from friends.js and specifically the scores of each friend
         console.log(req.body.scores);
         var newFriendScores = req.body.scores;
         var scoresArray = [];
         var bestMatch = 0;
-
+        // loop through all of the friends and use absolute value to 
+        // find total difference between user array of scores and each friend array
         for(var i = 0; i < friendsList.length; i++) {
             var scoresDiff = 0;
             for (var j = 0; j < newFriendScores.length; j++) {
                 scoresDiff += (Math.abs(parseInt(friendsList[i].scores[j]) - parseInt(newFriendScores[j])));
             }
+            //push to the new scoresArray
             scoresArray.push(scoresDiff);
         }
-
+        // loop through the new array, and compare to all current friends to find the best match
         for (var i = 0; i < scoresArray.length; i++) {
             if(scoresArray[i] <= scoresArray[bestMatch]){
                 bestMatch = i;
